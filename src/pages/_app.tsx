e35cff2e24bb8b81461493b0c1dab7f56b270d75
thangVjppro <hsgsdominate@gmail.com>
2023-02-24 20:41:35 +0100

    tung

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import 'tailwindcss/tailwind.css'
+import '../styles/App.css'
 import { ThemeProvider } from 'next-themes'
-
+import Head from 'next/head'
 const App = ({ Component, pageProps }) => {
 	return (
 		<ThemeProvider attribute="class">
+<Head>
+</Head>
 			<Component {...pageProps} />
 		</ThemeProvider>
 	)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,11 @@ import { APP_NAME } from '@/lib/consts'
 import ThemeSwitcher from '@/components/ThemeSwitcher'
 import { BookOpenIcon, CodeBracketIcon } from '@heroicons/react/24/outline'
 import client from '@/lib/prismadb'
-
+import App from "./app"
 const Home: FC = () => {
-	return <div>haii</div>
+	return <div>
+		<App/>
+	</div>
 }
 
 export default Home
