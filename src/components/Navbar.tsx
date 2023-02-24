f8b2f0ac8ad36b2c92c9abd7462c96ac43adf852
thangVjppro <hsgsdominate@gmail.com>
2023-02-24 21:57:52 +0100

    Update intro of team

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
 export default function Navbar() {
     return (
       <nav className="navbar navbar-default navbar-fixed-top transparent">
+          <h1 className="navbar-brand">Parkable</h1>
+
         <div className="container-fluid">
-          
-          <div className="navbar-header">
+            <div className="navbar-header">
             <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
               <span className="sr-only">Toggle navigation</span>
               <span className="icon-bar"></span>
---
+++ src/pages/app_dashboard.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Navbar from "../components/Navbar"
+import Parking from "../components/Parking"
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+            <Parking/>
+        </div>
+    )
+}
