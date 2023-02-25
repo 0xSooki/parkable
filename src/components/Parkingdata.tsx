dfaadf198e238893ab86ba1f4cf243710f49b498
thangVjppro <hsgsdominate@gmail.com>
2023-02-25 09:07:54 +0100

    Update parking status map

---
+++ src/components/Parkingdata.tsx
@@ -0,0 +1,82 @@
+export default [
+    {
+        id: 1,
+        on: true
+    },   
+    {
+        id: 2,
+        on: false
+    },   
+    {
+        id: 3,
+        on: true
+    },   
+    {
+        id: 4,
+        on: true
+    },   
+    {
+        id: 5,
+        on: false
+    },
+    {
+        id: 5,
+        on: true
+    },      
+    {
+        id: 7,
+        on: false
+    },   
+    {
+        id: 8,
+        on: false
+    },   
+    {
+        id: 9,
+        on: false
+    },   
+    {
+        id: 10,
+        on: false
+    },   
+    {
+        id: 11,
+        on: false
+    },   
+    {
+        id: 12,
+        on: false
+    },   
+    {
+        id: 13,
+        on: false
+    },   
+    {
+        id: 14,
+        on: false
+    },   
+    {
+        id: 15,
+        on: false
+    },   
+    {
+        id: 16,
+        on: false
+    },   
+    {
+        id: 17,
+        on: false
+    },   
+    {
+        id: 18,
+        on: false
+    },   
+    {
+        id: 19,
+        on: false
+    },   
+    {
+        id: 20,
+        on: false
+    },   
+]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,9 @@ import ThemeSwitcher from '@/components/ThemeSwitcher'
 import { BookOpenIcon, CodeBracketIcon } from '@heroicons/react/24/outline'
 import client from '@/lib/prismadb'
 import App from "./app"
-
 const Home: FC = () => {
-	const { data: session } = useSession()
-	if (session) {
-		return (
-			<>
-				Signed in as {session.user.email} <br />
-				<button onClick={() => signOut()}>Sign out</button>
-        <App/>
-			</>
-		)
-	}
-	return (
-		<>
-			Not signed in <br />
-			<button onClick={() => signIn()}>Sign in</button>
-		</>
-	)
+	return <div>
+		<App/>
+	</div>
 }
-
 export default Home
